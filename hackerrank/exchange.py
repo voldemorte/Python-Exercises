02631a0f97183a4251d88c9d79cfd423feb3461e
Nikhil <nikhil.chaudhari@india.com>
2014-12-06 18:44:34 +0530

    hackerrank exercises

---
+++ hackerrank/exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# https://www.hackerrank.com/challenges/interchange-two-numbers
+
+# You are given two integers. Store them into two variables and exchange
+# them. Can it get any easier! Make sure you use tuple to do the task,
+# rather than using any fancy logic. Print the two numbers.
+
+num1 = raw_input()
+num2 = raw_input()
+
+print num2
+print num1
+
+## This is just plain foolish :P

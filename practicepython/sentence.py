bac06ece63d7cb41bf55faee7a58797e3e536224
Nikhil <nikhil.chaudhari@india.com>
2014-12-06 17:05:35 +0530

    Ex 15

---
+++ practicepython/sentence.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#Write a program (using functions!) that asks the user for a long string #containing multiple words. Print back to the user the same string, except #with the words in backwards order. For example, say I type the string:
+#
+#My name is Michele
+#Then I would see the string:
+#
+#Michele is name My
+#shown back to me.
+
+lst = str(raw_input('Enter your string: ')).split()
+
+print ' '.join(lst[::-1])
+
+#for i in lst[::-1]:
+#  print i + ' ',

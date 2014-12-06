5e5198911a9fec5a756140c9086b6d54a6936e47
Nikhil <nikhil.chaudhari@india.com>
2014-12-06 17:56:32 +0530

    Hackerrank challenges

---
+++ hackerrank/README.md
@@ -0,0 +1,2 @@
+This is tutorials solutions from hackerrank site.
+Will have problem statements in the comment at the start of the code.
---
+++ hackerrank/name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# https://www.hackerrank.com/challenges/whats-your-name
+
+#Lets learn the basics of Python! You are given the First name and the Last
+#name of a person. Your task is to read them and print
+
+#Hello firstname lastname! You just delved into python.
+
+#Its that simple!
+
+first = str(raw_input())
+last = str(raw_input())
+
+print "Hello %s %s! You just delved into python." % (first, last)
